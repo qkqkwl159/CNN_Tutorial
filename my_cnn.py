9e25dc7829725a0af194a3c19d89fc17c8d91b0e
qkqkwl159 <qkqkwl147@gmail.com>
2023-09-15 04:08:37 +0900

    add comment

--- my_cnn.py
+++ my_cnn.py
@@ -1,3 +1,4 @@
+## 1. 라이브러리 및 데이터셋 불러오기
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models, datasets
@@ -5,7 +6,7 @@ from tensorflow.keras import layers, models, datasets
 # CIFAR-10 데이터셋 불러오기
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 
-# 이미지 정규화
+## 2. CNN 모델 구성하기
 train_images, test_images = train_images / 255.0, test_images / 255.0
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
@@ -18,16 +19,17 @@ model = models.Sequential([
     layers.Dense(10)
 ])
 
+## 3. Model Compile
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-
+## 4. Model 학습
 
 
 history = model.fit(train_images, train_labels, epochs=10, 
                     validation_data=(test_images, test_labels))
-
+## 5. 성능 평가 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
 model.save("tuto_model1")
